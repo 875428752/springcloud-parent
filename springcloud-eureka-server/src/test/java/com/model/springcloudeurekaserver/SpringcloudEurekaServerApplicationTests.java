48b4377d22c5dd911a24fd9081902f5281c27270
jin <875428752@qq.com>
2019-03-25 16:36:11 +0800

    jin init 190325

---
+++ springcloud-eureka-server/src/test/java/com/model/springcloudeurekaserver/SpringcloudEurekaServerApplicationTests.java
@@ -0,0 +1,22 @@
+package com.model.springcloudeurekaserver;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.management.Agent;
+
+import javax.naming.Name;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class SpringcloudEurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
