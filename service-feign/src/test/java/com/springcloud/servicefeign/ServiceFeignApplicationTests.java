073d404879cd131982af7a4fa7273e3916900857
jin <875428752@qq.com>
2019-03-26 16:12:27 +0800

    SpringCloud Finchley

--- service-feign/src/test/java/com/springcloud/servicefeign/ServiceFeignApplicationTests.java
+++ service-feign/src/test/java/com/springcloud/servicefeign/ServiceFeignApplicationTests.java
@@ -5,18 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ServiceFeignApplicationTests {
 
     @Test
     public void contextLoads() {
-        List<String> stringList = new ArrayList<>();
-        ArrayList<String> stringList2 = new ArrayList<>();
-
     }
-
 }
