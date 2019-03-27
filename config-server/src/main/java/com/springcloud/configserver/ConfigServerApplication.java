f1150e4fd144f17b39b9fcd8233d4db1ee353b64
jin <875428752@qq.com>
2019-03-27 16:35:14 +0800

    SpringCloud Finchley

--- config-server/src/main/java/com/springcloud/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/springcloud/configserver/ConfigServerApplication.java
@@ -1,8 +1,10 @@
 package com.springcloud.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -11,5 +13,4 @@ public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
