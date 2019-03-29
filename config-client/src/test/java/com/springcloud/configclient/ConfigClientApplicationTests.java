193d9465c252804bd5cc8ab61a6d17c70f237bbe
jin <87542875@qq.com>
2019-03-29 11:16:20 +0800

    SpringCloud Finchley

--- config-client/src/test/java/com/springcloud/configclient/ConfigClientApplicationTests.java
+++ config-client/src/test/java/com/springcloud/configclient/ConfigClientApplicationTests.java
@@ -2,15 +2,28 @@ package com.springcloud.configclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration
 public class ConfigClientApplicationTests {
 
+    @Autowired
+    TestRestTemplate testRestTemplate;
     @Test
     public void contextLoads() {
+
+        System.out.println("-------------kaishi");
+        testRestTemplate.postForObject("http://localhost:8881/actuator/bus-refresh",null,Map.class);
+        System.out.println("-------------stop");
     }
 
 }
