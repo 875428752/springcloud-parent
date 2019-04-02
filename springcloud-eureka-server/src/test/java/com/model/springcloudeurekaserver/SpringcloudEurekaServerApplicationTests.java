4a820b09155366e8aaf83ebc499e88d25e71cdb4
jin <875428752@qq.com>
2019-04-02 11:16:58 +0800

    SpringCloud Finchley

--- springcloud-eureka-server/src/test/java/com/model/springcloudeurekaserver/SpringcloudEurekaServerApplicationTests.java
+++ springcloud-eureka-server/src/test/java/com/model/springcloudeurekaserver/SpringcloudEurekaServerApplicationTests.java
@@ -9,11 +9,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 import sun.management.Agent;
 
 import javax.naming.Name;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@Ignore
+//@SpringBootTest
+//@Ignore
 public class SpringcloudEurekaServerApplicationTests {
+
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("a", null);
+        Iterator<Map.Entry<Object, Object>> iterator = map.entrySet().iterator();
+        ConcurrentHashMap<Object, Object> objectObjectConcurrentHashMap = new ConcurrentHashMap<>();
+        objectObjectConcurrentHashMap.put("a", null);
+    }
 
     @Test
     public void contextLoads() {
